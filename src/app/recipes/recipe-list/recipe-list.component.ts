5be14176f764a4b85a4e5bc636a2b9b87c49d735
SYILMAZ\Administrator <mchiang@rentreporters.com>
2016-09-27 18:24:52 -0700

    migrated to newest angular cli

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from '../recipe';
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html',
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipe=new Recipe('Dummy', 'Dummy', 'http://previewcf.turbosquid.com/Preview/2014/05/21__03_48_03/dummy_1.jpg73a34682-6a0e-42f8-bbeb-b21eaa495913Original.jpg');
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe){
+    this.recipeSelected.emit(recipe);
+  }
+}
